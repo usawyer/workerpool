d6f205b39d4b27759545674060142ea0ec017a0f
Шевчук Ирина <shevchuk.irina5@wb.ru>
2024-11-04 23:31:46 +0700

    feature: added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+### Description
+This project focuses on developing a basic worker pool in Go with the capability to dynamically add and remove workers. The system handles input data (strings) sent to a channel, which is then processed by the workers.
+
+### Technologies
+Go v1.23.2
+
+### Installation and Running
+`make run`
--- internal/workerpool/workerpool.go
+++ internal/workerpool/workerpool.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+// WorkerPool
+// workers - количество воркеров
+// jobChan - канал с джобами
+// quitChan - канал для завершения работы воркера
+// wg - примитив синхронизации
 type WorkerPool struct {
 	workers  atomic.Int64
 	jobChan  chan string
@@ -15,6 +20,7 @@ type WorkerPool struct {
 	wg       sync.WaitGroup
 }
 
+// NewWorkerPool создание нового ВоркерПула
 func NewWorkerPool() *WorkerPool {
 	return &WorkerPool{
 		jobChan:  make(chan string),
@@ -22,6 +28,7 @@ func NewWorkerPool() *WorkerPool {
 	}
 }
 
+// StartWorker запускает работу воркера
 func (wp *WorkerPool) StartWorker(id int) {
 	for {
 		select {
@@ -38,6 +45,7 @@ func (wp *WorkerPool) StartWorker(id int) {
 	}
 }
 
+// AddWorker добавляет нового воркера
 func (wp *WorkerPool) AddWorker() {
 	wp.wg.Add(1)
 	id := wp.workers.Add(1)
@@ -48,10 +56,12 @@ func (wp *WorkerPool) AddWorker() {
 	slog.Info(fmt.Sprintf("Worker %d created", id))
 }
 
+// AddJob добавляет новую джобу
 func (wp *WorkerPool) AddJob(job string) {
 	wp.jobChan <- job
 }
 
+// RemoveWorker удаляет воркера из пула
 func (wp *WorkerPool) RemoveWorker() {
 	if wp.workers.Load() == 0 {
 		slog.Info("No workers to remove")
@@ -61,24 +71,29 @@ func (wp *WorkerPool) RemoveWorker() {
 	wp.quitChan <- struct{}{}
 }
 
+// StopAll останавливает работу всех воркеров
 func (wp *WorkerPool) StopAll() {
 	close(wp.quitChan)
 	wp.WaitAll()
 	wp.CloseJobChannel()
 }
 
+// WaitAll дожидается окончания выполнения джобов воркерами
 func (wp *WorkerPool) WaitAll() {
 	wp.wg.Wait()
 }
 
+// CloseJobChannel закрывает канал джобов
 func (wp *WorkerPool) CloseJobChannel() {
 	close(wp.jobChan)
 }
 
+// GetWorkersNum возвращает текущее количество воркеров
 func (wp *WorkerPool) GetWorkersNum() int64 {
 	return wp.workers.Load()
 }
 
+// GetJob возвращает текущий джоб
 func (wp *WorkerPool) GetJob() string {
 	return <-wp.jobChan
 }
